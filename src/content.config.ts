7306d7698d3995fa6f3c916ad8badc8f8f4d66d9
garesoft <subcity@me.com>
2025-08-16 20:51:10 -0400

    poopy

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,12 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
+const resources = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/resources"}),
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+    }),
+})
 
-export const collections = { blog };
+export const collections = { resources };
